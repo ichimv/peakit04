66841db24464975cf46ee73ffc6daecbf4ec19d0
sichim <sebastian_ichim@trimble.com>
2021-10-10 16:37:52 +0300

    Validate layers implementation

--- Vulkan_basic_code/2_create_instance/2_create_instance.cpp
+++ Vulkan_basic_code/2_create_instance/2_create_instance.cpp
@@ -52,7 +52,7 @@ private:
    void createInstance() {
       VkApplicationInfo appInfo{};
       appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-      appInfo.pApplicationName = "Hello Triangle";
+      appInfo.pApplicationName = "Hello Vulkan";
       appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
       appInfo.pEngineName = "No Engine";
       appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
