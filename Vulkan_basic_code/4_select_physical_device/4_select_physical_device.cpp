db9318f1b9c642771ddac82de5fd66624794b5e3
sichim <sebastian_ichim@trimble.com>
2021-10-17 15:23:59 +0300

    Added all examples and shaders

--- Vulkan_basic_code/4_select_physical_device/4_select_physical_device.cpp
+++ Vulkan_basic_code/4_select_physical_device/4_select_physical_device.cpp
@@ -103,7 +103,7 @@ private:
 
       VkApplicationInfo appInfo{};
       appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-      appInfo.pApplicationName = "Hello Triangle";
+      appInfo.pApplicationName = "Hello Vulkan";
       appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
       appInfo.pEngineName = "No Engine";
       appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
---
+++ Vulkan_basic_code/9_create_graphics_pipeline/Shaders/compile.bat
@@ -0,0 +1,3 @@
+d:/VulkanSDK/1.2.189.2/Bin32/glslc.exe shader_base.vert -o vert.spv
+d:/VulkanSDK/1.2.189.2/Bin32/glslc.exe shader_base.frag -o frag.spv
+pause
